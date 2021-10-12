ccb1c024e94b5dbbcfe6a930022768991c450e30
AWSMProdigy <StraightAKyle@gmail.com>
2021-10-12 10:09:31 -0400

    Added new tooltips

--- metroid-dread-map/src/Areas/artaria.js
+++ metroid-dread-map/src/Areas/artaria.js
@@ -10,10 +10,12 @@ function artaria(){
             <section className="buttonContainer">
                 <Tooltip src={screwAttack} id="energyRefill1" text="Use this to refill energy" alt="energyRefill1"></Tooltip>
                 <Tooltip src={screwAttack} id="ammoRefill1" text="Use this to refill missiles and power bombs" alt="ammoRefill1"></Tooltip>
-                <Tooltip src={screwAttack} id="energyPiece1" text="Collect 4 to increase energy by 100" alt="energyPiece1"></Tooltip>
+                <Tooltip src={screwAttack} id="energyTank1" text="Increase energy by 100" alt="energyTank1"></Tooltip>
                 <Tooltip src={screwAttack} id="saveStation1" text="Save your file here" alt="saveStation1"></Tooltip>
                 <Tooltip src={screwAttack} id="missilePickup1" text="Increase missile count by 2" alt="missilePickup1"></Tooltip>
                 <Tooltip src={screwAttack} id="missilePickup2" text="Increase missile count by 2" alt="missilePickup2"></Tooltip>
+                <Tooltip src={screwAttack} id="missilePickup3" text="Increase missile count by 2" alt="missilePickup3"></Tooltip>
+                <Tooltip src={screwAttack} id="energyPiece1" text="Collect 4 to increase energy by 100" alt="energyPiece1"></Tooltip>
             </section>
         </>
     )      
